e85ab72c2fbf1b9462df2202d77b607665ec48a3
Parv Khandelwal <parv10046@gmail.com>
2026-04-05 21:19:37 +0530

    Initial commit

---
+++ src/main/java/com/parv/high_concurrency_flash_sale_system/HighConcurrencyFlashSaleSystemApplication.java
@@ -0,0 +1,13 @@
+package com.parv.high_concurrency_flash_sale_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighConcurrencyFlashSaleSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighConcurrencyFlashSaleSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parv/high_concurrency_flash_sale_system/HighConcurrencyFlashSaleSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parv.high_concurrency_flash_sale_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighConcurrencyFlashSaleSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
